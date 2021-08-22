217b7c5bcc7aacdd240c3afa08bdb34c26588dfd
zulazri <zulazrizulkarnain@gmail.com>
2021-08-23 04:12:04 +0800

    test procedure 23/08/21

--- include/booking.php
+++ include/booking.php
@@ -95,8 +95,6 @@ if (isset($_POST['bookslot']))
                             {
                                 echo '<script type="text/javascript">alert("Something went wrong!");location="../customer/boarding.php";</script>';
                             }
-                        }else {
-                          echo "apa ni babi";
                         }
                     }
                     else
